1f0b6d9078b4382b6fbeacd26660409cb6b3a81f
zhangboheng <43784936+zhangboheng@users.noreply.github.com>
2020-10-20 00:23:00 +0800

    Add files via upload

---
+++ Simple Calculators Numbers Sum/js/sum.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("load",function(){
+	var sumBtn = document.getElementById("sumBtn");
+	var youInput = document.getElementById("num");
+	youInput.addEventListener("keyup",function(){
+		this.value = this.value.replace(/[^(\d)|(\+)]/, "");
+	})
+	sumBtn.addEventListener("click",function(){
+		let sum = 0;
+        let arr = document.getElementById("num").value.split("+");
+        this.value = this.value.replace(/[^(\d)|(\+)]/,"");
+		sum = arr.reduce((x,y) => Number(x) + Number(y));
+        alert(sum);
+	})
+})
+	
