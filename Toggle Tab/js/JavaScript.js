c0b9e320dce552376e2695564cff4e03a70bb9d2
zhangboheng <zhangboheng827@gmail.com>
2020-10-23 12:30:41 +0800

    Update something

---
+++ Toggle Tab/js/JavaScript.js
@@ -0,0 +1,18 @@
+﻿var tabs = document.getElementById("tab").getElementsByTagName("li");
+var divs = document.getElementById("mc").getElementsByTagName("div");
+for (var i = 0; i < tabs.length; i++) {
+        tabs[i].onmouseover = function () { change(this); }
+    }
+
+function change(obj) {
+    for (var i = 0; i < tabs.length; i++) {
+        if (tabs[i] === obj) {
+            tabs[i].className = "tab_selected";
+            divs[i].className = "main_selected";
+        }
+        else {
+            tabs[i].className = "";
+            divs[i].className = "";
+        }
+    }
+}
