638f419c53126cf7d86ff84265b77b52447c75bd
zhangboheng <zhangboheng827@gmail.com>
2020-10-24 17:07:47 +0800

    Add one more method

--- Slider Auto Show/js/home.js
+++ Slider Auto Show/js/home.js
@@ -1,3 +1,5 @@
+//Sovle 1
+/*
 var slideIndex = 0;
 showSlides();
 
@@ -11,4 +13,17 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+*/
+
+//Sovle 2
+
+var slides = document.querySelectorAll('.slider');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].style.display = 'block';
 }
