43a3c91cb813d690e2249d183dc7100d7a32d31c
zhangboheng <zhangboheng827@gmail.com>
2020-11-01 21:11:34 +0800

    Update Javascript.js

--- DOM/Creat New Nodes/js/Javascript.js
+++ DOM/Creat New Nodes/js/Javascript.js
@@ -1,11 +1,12 @@
 //Javascript
-/*
-var element = document.getElementById("cra");
+/*var element = document.getElementById("cra");
 element.onclick=function(){
 	var para = document.createElement("p");
 	var node = document.createTextNode("Hello Third");
+	var pren = document.getElementsByTagName("p")[0];
 	para.appendChild(node);
 	this.appendChild(para);
+	this.insertBefore(para,pren);
 }
 */
 
@@ -15,5 +16,6 @@ $("document").ready(function(){
 		var text1 = "<p>Hello Third</p>";
 		var text2 = $("<p></p>").text("Hello Fourth");
 		$(this).append(text1,text2);
+		//$(this).prepend(text1,text2);
 	})
 })
