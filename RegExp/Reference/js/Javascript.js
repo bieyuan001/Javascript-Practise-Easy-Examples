fffa66758c4f6874f172fc7690e12b2cd7cdfa6b
zhangboheng <zhangboheng827@gmail.com>
2020-11-01 19:00:21 +0800

    Add jQuery Version

--- RegExp/Reference/js/Javascript.js
+++ RegExp/Reference/js/Javascript.js
@@ -1,3 +1,5 @@
+//Javascript
+/*
 var getTop = document.getElementsByClassName("retop")[0];
 window.onscroll = function(){
 	if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
@@ -9,4 +11,20 @@ window.onscroll = function(){
 getTop.addEventListener("click", function(){
 	document.body.scrollTop = 0;//Safari to use.
 	document.documentElement.scrollTop = 0;//Chrome, Firefox, Internet Explore and Opra.
-})// JavaScript Document
+})
+*/
+
+//jQuery
+$(document).ready(function(){
+	$(window).scroll(function(){
+		if($(this).scrollTop()>=30){
+			$(".retop").fadeIn();
+		}else{
+			$(".retop").fadeOut();
+		}
+	})
+	$(".retop").click(function(){
+		$("html, body").animate({scrollTop:0},200);
+	})
+})
+
