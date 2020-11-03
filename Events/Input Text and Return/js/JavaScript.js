aa796026fa19f8fba4db64e84ad4c9d89224cac7
zhangboheng <zhangboheng827@gmail.com>
2020-11-03 08:20:20 +0800

    Add jQuery Version

--- Events/Input Text and Return/js/JavaScript.js
+++ Events/Input Text and Return/js/JavaScript.js
@@ -1,4 +1,6 @@
-﻿var myFun = (a, b) => {
+﻿//Javascript
+/*
+var myFun = (a, b) => {
     alert(a.value+" "+b.value);
 	document.getElementsByTagName("span")[0].style.display = "none";
 	document.getElementsByTagName("span")[1].style.display = "none";
@@ -9,5 +11,42 @@ window.addEventListener("load" , () => {
     oBtn.onclick = () => {
         myFun(oInput[0], oInput[1]);
     }
+})
+*/
+
+//jQuery
+$(document).ready(function(){
+	$("input:eq(0)").focus(function(){
+		$("span:eq(0)").css({display:"none"});
+		$("input:eq(0)").blur(function(){
+			if($(this).val()===""){$(this).focus()} 
+		})
+	})
+	$("input:eq(0)").blur(function(){
+		if($(this).val()===""){$("span:eq(0)").css({display:"block"});}
+		else{$("span:eq(0)").css({display:"none"});}
+	})
+	$("input:eq(0)").keyup(function(){
+		/\d$/.test($(this).val())? $(this).val() : this.value="";
+	})
+	$("input:eq(1)").focus(function(){
+		$("span:eq(1)").css({display:"none"});
+		$("input:eq(1)").blur(function(){
+			if($(this).val()===""){$(this).focus()} 
+		})
+	})
+	$("input:eq(1)").blur(function(){
+		if($(this).val()===""){$("span:eq(1)").css({display:"block"});}
+		else{$("span:eq(1)").css({display:"none"});}
+	})
+	$("input:eq(1)").keyup(function(){
+		/\d$/.test($(this).val())? $(this).val() : this.value="";
+	})
+	$("button").click(function(){
+		alert(Number($("input:eq(0)").val())+Number($("input:eq(1)").val()));
+		$("input:eq(0)").val("");
+		$("input:eq(1)").val("");
+		$("span").css({display:"block"});
+	})
 })
   
