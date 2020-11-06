6ed951efc64aa8b9b2cf9c6b7b4c64cc0f91ffa4
zhangboheng <zhangboheng827@gmail.com>
2020-11-06 17:52:11 +0800

    Update Somethings

--- jQuery/Selectors Practise/js/Javascript.js
+++ jQuery/Selectors Practise/js/Javascript.js
@@ -30,4 +30,46 @@ $(document).ready(function(){
 		$(this).animate({marginLeft:"40px"},200);
 		$(this).animate({marginLeft:"0px"},200);
 	});
+	$("span:first-of-type").click(function(){
+		$(this).wrapInner("<del></del>");
+	});
+	$("span:last-of-type").click(function(){
+		$(this).wrapInner("<b></b>");
+	});
+	$("p:nth-child(1)").click(function(){
+		$(this).append("<span> fuck you!</span>");
+	});
+	$("p:nth-last-child(2)").click(function(){
+		$(this).prepend("<span> fuck you!</span>");
+	});
+	$("span:nth-of-type(1)").click(function(){
+		$("<span> Hello </span>").prependTo(this);
+	});
+	$("span:nth-last-of-type(1)").one("click",function(){
+		$("<span> Bye </span>").appendTo(this);
+	});
+	$("span:only-child").one("click",function(){
+		$("<span> Boy </span>").appendTo(this);
+	});
+	$("span:only-of-type").one("click",function(){
+		$("h1").remove();
+	});
+	$("#textstyle1>p").one("click",function(){
+		$("h1").remove();
+	});
+	$("#textstyle1 p").click(function(){
+		$("this").removeClass("h1");
+	})
+	$("#textstyle1 ~ div").click(function(){
+		$("h1").remove("h1");
+	})
+	$(".textstyle p : eq(0)").click(function(){
+		$("h1").remove("h1");
+	})
+	$(".textstyle p : gt(0)").click(function(){
+		$("h1").remove("h1");
+	})
+	$(".textstyle p : lt(1)").click(function(){
+		$("h1").remove("h1");
+	})
 })
