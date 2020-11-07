3b82d65fd0a04e462fbd3f0b38121331e5542847
zhangboheng <zhangboheng827@gmail.com>
2020-11-07 13:21:17 +0800

    Update A jQuery Example
    About Tab List

---
+++ jQuery/Search Tab/js/Javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$("#search-bar li:eq(0)").click(function(){
+		$("p").hide();
+		$("p:eq(0)").show(500);
+	})
+	$("#search-bar li:eq(1)").click(function(){
+		$("p").hide();
+		$("p:eq(1)").show(500);
+	})
+	$("#search-bar li:eq(2)").click(function(){
+		$("p").hide();
+		$("p:eq(2)").show(500);
+	})
+	$("#search-bar li:eq(3)").click(function(){
+		$("p").hide();
+		$("p:eq(3)").show(500);
+	})
+})// JavaScript Document
