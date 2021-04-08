95812b012fc536434ae149484240515c81b39c54
zhangboheng <43784936+zhangboheng@users.noreply.github.com>
2021-04-08 11:06:17 +0800

    API solve 'Access-Control-Allow-Origin'

--- API/Get Pixivweb Pictures/fetch.js
+++ API/Get Pixivweb Pictures/fetch.js
@@ -1,5 +1,4 @@
 var url = 'https://api.pixivweb.com/api.php?return=json';
-var proxy = 'https://cors-anywhere.herokuapp.com/';
 var puts = ""
 
 $(document).ready(function() {
@@ -7,8 +6,14 @@ $(document).ready(function() {
 });
 
 function putImage() {
+    $.ajaxSetup({
+        xhrFields: {
+            withCredentials: true
+        },
+        async: false
+    });
     $.ajax({
-        url: proxy + url,
+        url: url,
         headers: {
             'Content-Type': 'text/json;charset=UTF-8',
         },
